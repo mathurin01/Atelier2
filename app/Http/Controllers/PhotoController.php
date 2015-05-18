52f7c03dde54bad3ee5846e56aeaa0b0c4ffdb42
mathurin <jpetiteau@gmail.com>
2015-05-18 14:52:51 +0200

    Délégation des taches (injection de dépendances, interface) modification des providers

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -1,6 +1,9 @@
-<?php namespace App\Http\Controllers;
+<?php
+
+namespace App\Http\Controllers;
 
 use App\Http\Requests\ImagesRequest;
+use App\Gestion\PhotoGestionInterface;
 
 class PhotoController extends Controller {
 
@@ -9,27 +12,17 @@ class PhotoController extends Controller {
         return view('photo');
     }
 
-    public function postForm(ImagesRequest $request)
+    public function postForm(
+        ImagesRequest $request,
+        PhotoGestionInterface $photogestion)
     {
-        $image = $request->file('image');
 
-        if($image->isValid())
-        {
-            $chemin = config('images.path');
-
-            $extension = $image->getClientOriginalExtension();
-
-            do {
-                $nom = str_random(10) . '.' . $extension;
-            } while(file_exists($chemin . '/' . $nom));
-
-            if($image->move($chemin, $nom)) {
-                return view('photo_ok');
-            }
+        if($photogestion->save($request->file('image'))) {
+            return view('photo_ok');
         }
-
         return redirect('photo/form')
             ->with('error','Désolé mais votre image ne peut pas être envoyée !');
+
     }
 
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,9 @@ class AppServiceProvider extends ServiceProvider {
 	{
 		$this->app->bind(
 			'Illuminate\Contracts\Auth\Registrar',
-			'App\Services\Registrar'
+			'App\Services\Registrar',
+            'App\Gestion\PhotoGestionInterface',
+            'App\Gestion\PhotoGestion'
 		);
 	}
 
